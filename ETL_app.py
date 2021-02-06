9296af610810b097d34c4e5e3af8754dc12581e2
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-02-06 14:25:59 -0500

    endpoint

--- ETL_app.py
+++ ETL_app.py
@@ -18,7 +18,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/happiness")
+@app.route("/happiness", endpoint = "HappyEnd")
 def happiness():
     print("Server received request for POSTGRES connection...")
     connection_string = f"{user_nm}:{user_pw}@localhost:{user_port}/world_db"
@@ -30,6 +30,18 @@ def happiness():
     results = html_table
     return render_template("data.html", info = results)
 
+
+@app.route("/alcohol", endpoint = "AlcoEnd")
+def happiness():
+    print("Server received request for POSTGRES connection...")
+    connection_string = f"{user_nm}:{user_pw}@localhost:{user_port}/world_db"
+    engine = create_engine(f'postgresql://{connection_string}')
+    connection = engine.connect()
+    df = pd.read_sql("SELECT * FROM alcohol_consumption", connection)
+    connection.close()
+    html_table = df.to_html(index=False, header=True, border=1, justify = 'left',classes="bg-light table table-striped table-bordered")
+    results = html_table
+    return render_template("data.html", info = results)
 
 
 if __name__ == "__main__":
