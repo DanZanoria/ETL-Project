73f800a1626be5328b9eff9c89fc8f2c37769f47
Mir <mirahmed07@gmail.com>
2021-02-06 13:25:20 -0500

    html fixed

--- ETL_app.py
+++ ETL_app.py
@@ -1,14 +1,7 @@
-import datetime as dt
-import numpy as np
 import pandas as pd
 from config import user_nm, user_pw, user_port
 
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
+from sqlalchemy import create_engine
 from flask import Flask, render_template, redirect
 
 #################################################
@@ -25,19 +18,19 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/happiness')
+@app.route("/happiness")
 def happiness():
+    print("Server received request for POSTGRES connection...")
     connection_string = f"{user_nm}:{user_pw}@localhost:{user_port}/world_db"
     engine = create_engine(f'postgresql://{connection_string}')
     connection = engine.connect()
-    happiness_df = pd.read_sql("SELECT * FROM happiness", connection)
+    df = pd.read_sql("SELECT * FROM happiness", connection)
     connection.close()
-    redirect("/data")
-
-@app.route('/data')
-def data():
-    results = happiness_df
+    html_table = df.to_html(index=False, header=True, border=1, justify = 'left',classes="bg-light table table-striped table-bordered")
+    results = html_table
     return render_template("data.html", info = results)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
