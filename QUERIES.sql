7848c8996d5e49b2e9182bcf68c836cd6259b91d
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-02-09 18:36:56 -0500

    clean up

--- QUERIES.sql
+++ QUERIES.sql
@@ -32,3 +32,29 @@ INNER JOIN alcohol_consumption AS t2 ON t1.country = t2.country AND t2.year = 20
 INNER JOIN antidepressant_consumption AS t3 ON t1.country = t3.country
 WHERE t1.year = 2017
 ORDER BY 2;
+
+SELECT country, happiness_rank, happiness_score, gdp_per_capita, 
+family, life_expectancy, freedom, trust_government, generosity, dystopia_residual 
+FROM happiness 
+WHERE year = 2017 ORDER BY happiness_rank;
+
+
+SELECT country, happiness_rank, happiness_score, gdp_per_capita, 
+family, life_expectancy, freedom, trust_government, generosity, dystopia_residual 
+FROM happiness 
+WHERE year = 2017 ORDER BY happiness_rank
+
+
+SELECT T1.country, T2.ranking, T1.gender, T1.per_capita_consumption
+FROM (SELECT country, gender, AVG(per_capita_consumption) AS per_capita_consumption 
+FROM alcohol_consumption GROUP BY country, gender ORDER BY 3 DESC, 1) AS T1
+INNER JOIN (SELECT country, per_capita_consumption, 
+ROW_NUMBER () OVER (ORDER BY per_capita_consumption DESC) AS ranking 
+FROM (SELECT country, AVG(per_capita_consumption) AS per_capita_consumption 
+FROM alcohol_consumption GROUP BY country ORDER BY 2 DESC) AS T3) AS T2
+ON T1.country = T2.country ORDER BY 2
+
+SELECT country,daily_dosage_per_1k, year, notes 
+FROM antidepressant_consumption ORDER BY daily_dosage_per_1k DESC
+
+SELECT AVG(happiness_score) From happiness
